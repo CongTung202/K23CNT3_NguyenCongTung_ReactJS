78f651890b2872d3ee1b42a95e53a3641487680d
CongTung202 <nct30000@gmail.com>
2025-02-11 15:13:51 +0700

    Day 2

--- CongTung_Day2/nct-helloworld/src/App.js
+++ CongTung_Day2/nct-helloworld/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./crossover.jpg";
+import "./App.css";
+import nctCompo from "./components/nctCompo";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <h1>NguyenCongTung-K23CNT3-React</h1>
+      <nctCompo />
     </div>
   );
 }
